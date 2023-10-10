1062e6ca12c652226afe020f057c83e64a94b44b
Dmytro Petyshyn <dev6012@gmail.com>
2023-10-10 22:11:04 +0300

    cfg vercel

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 const { DEV_PORT, DB_HOST } = process.env;
-console.log('====>>>>>', process.env);
 
 mongoose.set('strictQuery', true);
 
