5e18f54a91483868eff1c2bd5a0e66650e4e4a03
Dmytro Petyshyn <dev6012@gmail.com>
2023-10-11 11:03:18 +0300

    vercel cfg

--- server/app.js
+++ server/app.js
@@ -9,6 +9,24 @@ const authRouter = require('./routes/api/auth');
 
 const app = express();
 
+const mongoose = require('mongoose');
+const { DEV_PORT, DB_HOST } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(DEV_PORT, () => {
+      console.log(`🛠️  Server is up and ready on port: ${DEV_PORT}`);
+    });
+  })
+  .then(() => console.log('🌐 Database connected successfully'))
+  .catch((error) => {
+    console.log('⚠️ Database conection failed:', error.message);
+    process.exit(1);
+  });
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
